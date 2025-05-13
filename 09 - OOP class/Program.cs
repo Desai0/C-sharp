a958497bf16966ffcabb794a9ca796f267cf8d11
Desai0 <kolobokevgenij83@gmail.com>
2025-05-13 14:46:24 +0300

    Pochti OOP

---
+++ 09 - OOP class/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Classs
+{
+    class Cat // public/internal
+    {
+        class CatData // Двигатель содержится в машине (Композиция)
+        { //  public/internal/protected/private
+
+        }
+        public int Whiskers { get; private set; } // было полем стало свойством
+        
+        public int GetSetWhiskers
+        {
+            get => Whiskers;
+            set {
+                if (value > 0)
+                    Whiskers = value;
+            }
+        }
+        
+        public string Tail { private get; set; } = "Black";
+
+        public void Lie()
+        {
+
+        }
+
+        public Cat() : this(4, "Black" { }
+        public Cat(int whiskers) {
+            Whiskers = whiskers;
+        }
+        public Cat(int whiskers, string tail) {
+            Whiskers = whiskers;
+            Tail = tail;
+        }
+
+        class Kitten : Cat
+        {
+            public Kitten() : base(4) { }
+        }
+    }
+}
---
+++ 09 - OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿//namespace MySpace1
+//{
+//    class Person { } // класс - это шаблон
+//}
+
+//namespace MySpace2
+//{
+//    namespace MS2
+//    {
+//        class Person { }
+//    }
+
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        MySpace1.Person person = new MySpace1.Person();
+//        MySpace2.MS2.Person person2 = new MySpace2.MS2.Person();
+//    }
+//}
+
+using _09___OOP.Models;
+using _09___OOP.Service;
+
+using cm = _09___OOP.Models;
+using cs = _09___OOP.Service;
+class Program
+{
+    static void Main()
+    {
+        //_09___OOP.Models.Cat cat;
+        cm.Cat cat1;
+        cs.Cat cat2;
+    }
+}
